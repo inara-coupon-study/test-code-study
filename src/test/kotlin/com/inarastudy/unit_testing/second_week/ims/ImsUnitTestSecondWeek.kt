f0f61d64402580b76ece34ba40fb254cf14b7a11
ims-idlenara <gudwnsrh@i-nara.co.kr>
2023-05-29 23:12:52 +0900

    테스트 커밋 작성

---
+++ src/test/kotlin/com/inarastudy/unit_testing/second_week/ims/ImsUnitTestSecondWeek.kt
@@ -0,0 +1,13 @@
+package com.inarastudy.unit_testing.second_week.ims
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImsUnitTestSecondWeek{
+
+    @Test
+    fun helloTest(){
+        println("hello")
+    }
+}
